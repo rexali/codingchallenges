d90a23c66a178c1e532dee7f03e17a463a7e4b27
Aliyu Bello <aliyubababello@gmail.com>
2022-12-29 13:45:03 +0100

    weirdAlgorithm written

--- weirdAlgorithm.js
+++ weirdAlgorithm.js
@@ -1,2 +1,25 @@
 // Weird Algorithm
 
+
+// 1. check that n is betw 1 and 100 inclusive
+
+function weirdAlgorithm(nn) {
+	// body...
+	var n;
+	if (nn % 2 === 0) {
+		n = nn/2;
+		console.log(n);
+		if (n !== 1 ) {
+			weirdAlgorithm(n);
+		}
+	} else {
+		n= nn*3+1
+		console.log(n);
+		if (n !== 1 ) {
+			weirdAlgorithm(n);
+		}
+	}
+
+   return;
+}
+weirdAlgorithm(3)
